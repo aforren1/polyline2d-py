720bee083b2ed7fc11b5180598a55b994e0ea8c4
Alex Forrence <alex.forrence@gmail.com>
2021-02-09 07:36:17 -0500

    build wheels

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import numpy as np
 
 import polyline2d as p2d
 
+lightred = 1, 0.2, 0, 0.5
+darkred = 1, 0.2, 0, 1
+green = 0, 1, 0, 1
 vs = '''
 #version 330
 in vec2 vertices;
@@ -41,16 +44,19 @@ ctx = mgl.create_context(require=330)
 ctx.viewport = (0, 0, 800, 800)
 ctx.enable(mgl.BLEND)
 ctx.blend_func = mgl.SRC_ALPHA, mgl.ONE_MINUS_SRC_ALPHA
+ctx.point_size = 10
 shader = ctx.program(vertex_shader=vs, fragment_shader=fs)
-lightred = 1, 0.2, 0, 0.5
-darkred = 1, 0.2, 0, 0.9
 
 pts = np.empty(6, dtype=[('x', 'f4'), ('y', 'f4')])
 pts['x'] = [-0.25, -0.25, 0.25, 0.0, 0.25, -0.4]
 pts['y'] = [-0.5, 0.5, 0.25, 0.0, -0.25, -0.25]
+
 ret = p2d.create(pts, 0.1, p2d.JointStyle.ROUND, p2d.EndCapStyle.SQUARE)
 vbo = ctx.buffer(ret)
 vao = ctx.vertex_array(shader, [(vbo, '2f', 'vertices')])
+
+orig = ctx.buffer(pts)
+vao_orig = ctx.vertex_array(shader, [(orig, '2f', 'vertices')])
 
 t0 = default_timer()
 while default_timer() < (t0 + 5):
@@ -59,5 +65,7 @@ while default_timer() < (t0 + 5):
     shader['color'].value = lightred
     vao.render(mgl.TRIANGLES)
     shader['color'].value = darkred
-    vao.render(mgl.LINE_STRIP)
+    vao.render(mgl.LINES)
+    shader['color'] = 0.1, 0.9, 0.1, 1
+    vao_orig.render(mgl.POINTS)
     glfw.swap_buffers(win)
